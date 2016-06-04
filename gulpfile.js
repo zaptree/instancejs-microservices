4ecf933c8f546fd4f0eba6eb87be80dc45f47ff4
Carolos <karoloskonst@gmail.com>
2016-06-04 16:54:30 -0400

    added gulp file and linting stuff

---
+++ gulpfile.js
@@ -0,0 +1,35 @@
+'use strict';
+
+var gulp = require('gulp');
+var mocha = require('gulp-mocha');
+var istanbul = require('gulp-istanbul');
+var jshint = require('gulp-jshint');
+
+var folders = {
+	test: ['lib/**/*.js', 'index.js'],
+	lint: ['lib/**/*.js', 'tests/lib/**/*.js', 'index.js']
+};
+
+gulp.task('test', ['lint'], function () {
+	return gulp.src(folders.test)
+		.pipe(istanbul()) // Covering files
+		.pipe(istanbul.hookRequire()) // Force `require` to return covered files
+		.on('finish', function () {
+			// this does not include subfolders so fixtures wont be considered test folder
+			gulp.src(['./tests/*.test.js'], {read: false})
+				.pipe(mocha())
+				.pipe(istanbul.writeReports()) // Creating the reports after tests ran
+				.pipe(istanbul.enforceThresholds({thresholds: {global: 100}})); // Enforce a coverage of at least 100%
+		});
+
+});
+
+gulp.task('lint', function () {
+	// https://github.com/jshint/jshint/blob/master/examples/.jshintrc
+	return gulp.src(folders.lint)
+		.pipe(jshint()) // Covering files
+		.pipe(jshint.reporter('jshint-stylish'))
+		.pipe(jshint.reporter('fail'));
+});
+
+gulp.task('default', ['test']);
